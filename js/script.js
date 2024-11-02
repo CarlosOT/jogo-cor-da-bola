1facb2dcf77068313e479c890ae3a70638c01056
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-02 20:57:49 -0300

    v2.5
    +ajuste mensagem final

--- js/script.js
+++ js/script.js
@@ -36,6 +36,11 @@ var ativarIntervalo = function() { //função 01
             botaoCompartilha.style.display = "block";
             linkCompartilha.href = `https://api.whatsapp.com/send?&text=t%C3%A1%20todo%20mundo%20jogando,%20eu%20consegui%20fazer%20${pontos}%20pontos%20%F0%9F%98%86%0A%0Aquero%20ver%20voc%C3%AA%20agora%20%F0%9F%91%87%F0%9F%8F%BC%0A%0Ahttps://carlosot.github.io/jogo-cor-da-bola`;
             backgroundBody.style.backgroundImage = "";
+            if(pontos >= 50){
+                msgFinal2.style.display = "block";
+            }else{
+                msgFinal1.style.display = "block";
+            }
         }
         let timeout;
         function fimJogo(){
@@ -59,6 +64,9 @@ let titulo = window.document.getElementById('titulo');
 let idFooter = document.getElementById('idFooter');
 let botaoCompartilha = document.getElementById('botao3');
 let linkCompartilha = document.getElementById('linkCompartilha');
+let msgFinal1 = document.getElementById('msgFinal1');
+let msgFinal2 = document.getElementById('msgFinal2');
+
 
 let nomeCor = ["BOLA AMARELA", "BOLA VERMELHA", "BOLA AZUL", "BOLA VERDE", "BOLA ROXA", "BOLA LARANJA"];
 let hexaCor = ["#ffff00", "#ff0000", "#0800ff", "#00ff08", "#e100ff", "#ff8c00"];
@@ -76,6 +84,8 @@ let temporizadorDesligado = false;
 
 idPonto.style.display = "none";
 botaoCompartilha.style.display = "none";
+msgFinal1.style.display = "none";
+msgFinal2.style.display = "none";
 backgroundBody.style.backgroundColor = `${contrastes[random1]}`;
 //bloco.style.backgroundColor = `${hexaCor[random1]}`;
 bloco.style.backgroundImage = `radial-gradient(circle, ${hexaCor[random1]},#000000)`;
